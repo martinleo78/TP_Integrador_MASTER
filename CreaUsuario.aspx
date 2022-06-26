5c5e5880b7efab0a043672dcc573abe44696dc9e
NicolasCimino <nico.cimino.90@gmail.com>
2022-06-25 21:24:16 -0300

    prueba de commit sobre CreaUsuario

--- CreaUsuario.aspx
+++ CreaUsuario.aspx
@@ -3,5 +3,18 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <div>
         <asp:Label ID="label_bienvenida" runat="server" Text="" CssClass="h3"></asp:Label>
+
+         <div class="mt-2">
+        <label>Ingrese Usuario</label>
+        <asp:TextBox id="txt_buscar_usuario" runat="server" />
+
+        <asp:Button Text="Buscar" runat="server" OnClick="buscar_click" />
+    </div>
+     <div class="mt-2">
+        <h4>Listado de usuarios</h4>
+        <asp:Panel ID="tablaUsuarios" runat="server">
+        </asp:Panel>
+    </div>
+
     </div>
 </asp:Content>
--- CreaUsuario.aspx.cs
+++ CreaUsuario.aspx.cs
@@ -4,11 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Text;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace TP_Integrador_Master
 {
     public partial class CreaUsuario : Page
     {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["miconexion"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
             if (Session["usuariologgeado"] != null)
@@ -19,6 +24,65 @@ namespace TP_Integrador_Master
             else
             {
                 Response.Redirect("Default.aspx");
+            }
+        }
+
+        protected void buscar_click(object sender, EventArgs e)
+        {
+            try
+            {
+                string usuario = txt_buscar_usuario.Text;
+                
+
+                if (String.IsNullOrEmpty(usuario))
+                {
+                    usuario = "";
+                }
+
+
+                string qry = "SELECT  U.UserName, P.Description " +
+                    "FROM  Users U LEFT JOIN UserPrivileges ON U.Id = UserPrivileges.UserID INNER JOIN " +
+                    "Privileges P ON UserPrivileges.PrivilegeID = P.Id " +
+                    "where U.UserName like '%" + usuario + "%' and " +
+                    "U.is_deleted=0;";
+
+                SqlCommand sqlCmd = new SqlCommand(qry, con);
+                con.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCmd);
+                DataTable dt = new DataTable();
+                dataAdapter.Fill(dt);
+                StringBuilder sb = new StringBuilder();
+                sb.Append("<table class=\"table  table-striped table-bordered text-center mt-4\">");
+                sb.Append("<thead >");
+                sb.Append("<tr class=\"table-dark\">");
+                sb.Append("<th scope=\"col\">Usuario</th>");
+                sb.Append("<th scope=\"col\">Permiso</th>");
+                sb.Append("</tr>");
+                sb.Append("</thead>");
+                sb.Append("<tbody>");
+
+                foreach (DataRow dr in dt.Rows)
+                {
+                    sb.Append("<tr>");
+                    foreach (DataColumn dc in dt.Columns)
+                    {
+                        sb.Append("<td>");
+                        sb.Append(dr[dc.ColumnName].ToString());
+                        sb.Append("</td>");
+                    }
+
+                    sb.Append("</tr>");
+                }
+                sb.Append("</tbody>");
+                sb.Append("</table>");
+                con.Close();
+
+                tablaUsuarios.Controls.Add(new Label { Text = sb.ToString() });
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
             }
         }
     }
