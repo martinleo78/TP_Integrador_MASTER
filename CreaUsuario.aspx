1ea5a51ce065c3f2536ea77483e3f243575727d8
Martin Spero <martin.spero@gmail.com>
2022-06-26 20:39:06 -0300

    listo CRUD usuarios

--- CreaUsuario.aspx
+++ CreaUsuario.aspx
@@ -1,20 +1,73 @@
 ﻿<%@ Page Title="Crea o Modificar Usuario" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CreaUsuario.aspx.cs" Inherits="TP_Integrador_Master.CreaUsuario" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <div>
-        <asp:Label ID="label_bienvenida" runat="server" Text="" CssClass="h3"></asp:Label>
-
-         <div class="mt-2">
-        <label>Ingrese Usuario</label>
-        <asp:TextBox id="txt_buscar_usuario" runat="server" />
-
-        <asp:Button Text="Buscar" runat="server" OnClick="buscar_click" />
+    <div class="p-2 m-2 border border-dark">
+        <h2>Crear Usuario</h2>
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">Nombre del usuario</label>
+            <asp:TextBox ID="txt_alta_usuario" Class="form-control w-25" runat="server" />
+            <asp:Label ID="label_error_nombre" class="text-danger" runat="server"></asp:Label>
+        </div>
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">Contraseña</label>
+            <asp:TextBox ID="txt_alta_password" Class="form-control w-25" TextMode="Password" runat="server" />
+            <asp:Label ID="label_error_pass" class="text-danger" runat="server"></asp:Label>
+        </div>
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">Confirmar Contraseña</label>
+            <asp:TextBox ID="txt_alta_password_confirm" Class="form-control w-25" TextMode="Password" runat="server" />
+        </div>
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">Permiso</label>
+            <div class="col-sm-10">
+                <asp:DropDownList id="lstpermisoscrear" runat="server">
+                </asp:DropDownList>
+            </div>
+            <div class="col-sm-10">
+                <asp:Button Text="Crear" Class="btn btn-outline-dark m-2 w-auto" runat="server" OnClick="crear_Click" />
+            </div>
+        </div>
     </div>
-     <div class="mt-2">
-        <h4>Listado de usuarios</h4>
-        <asp:Panel ID="tablaUsuarios" runat="server">
-        </asp:Panel>
-    </div>
+    <div class="p-2 m-2 border border-dark">
+        <h2>Borrar/Editar Usuario</h2>
+        <div class="mb-3 row">
+            <label class="col-sm-2 col-form-label">Buscar Usuario</label>
+            <asp:TextBox ID="txt_buscar_usuario" Class="form-control w-25" runat="server" />
+            <asp:Button Text="Buscar" Class="btn btn-outline-dark ms-1 w-auto" runat="server" OnClick="buscar_click" />
+        </div>
+        <div class="mt-2">
+            <h3>Usuario seleccionado</h3>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Usuario</label>
+                <asp:TextBox ID="text_usuario" Class="form-control-plaintext w-25" ReadOnly="true" runat="server" />
+            </div>
 
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label">Permiso</label>
+                <asp:TextBox ID="txt_permiso" Class="form-control-plaintext w-25" ReadOnly="true" runat="server" />
+            </div>
+            <div>
+                <div>
+                    <h3>Borrar usuario</h3>
+                    <asp:Button Text="Borrar" Class="btn btn-outline-dark m-2 w-auto" runat="server" OnClick="borrar_Click" />
+                    <asp:Label ID="label_error_borrar" class="text-danger" Text="" runat="server" />
+                </div>
+
+                <div>
+                    <h3>Editar permiso</h3>
+                    <div class="mb-3 row">
+                        <label class="col-sm-2 col-form-label">Nuevo Permiso</label>
+                        <div class="col-sm-10">
+                            <asp:DropDownList ID="lstpermisosactualizar" runat="server">
+                            </asp:DropDownList>
+                        </div>
+                        <div class="col-sm-10">
+                            <asp:Button Text="Editar" Class="btn btn-outline-dark m-2 w-auto" runat="server" OnClick="editar_Click" />
+                            <asp:Label ID="label_error_edit" class="text-danger" Text="" runat="server" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </asp:Content>
--- CreaUsuario.aspx.cs
+++ CreaUsuario.aspx.cs
@@ -16,14 +16,32 @@ namespace TP_Integrador_Master
         SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["miconexion"].ToString());
         protected void Page_Load(object sender, EventArgs e)
         {
+            /////////////////VERIFICO SI USUARIO LOGGEADO O NO
             if (Session["usuariologgeado"] != null)
             {
                 string usuariologgeado = Session["usuariologgeado"].ToString();
-                label_bienvenida.Text = "Bienvenido/a " + usuariologgeado;
             }
             else
             {
                 Response.Redirect("Default.aspx");
+            }
+            if (!Page.IsPostBack)
+            {
+                /////////////////CARGO DROPDOWNS DE PRIVILEGIOS
+                con.Open();
+                SqlCommand com = new SqlCommand("select *from Privileges", con);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                DataSet ds = new DataSet();
+                da.Fill(ds);
+                lstpermisoscrear.DataTextField = ds.Tables[0].Columns["Description"].ToString();
+                lstpermisoscrear.DataValueField = ds.Tables[0].Columns["Id"].ToString();
+                lstpermisoscrear.DataSource = ds.Tables[0];
+                lstpermisoscrear.DataBind();
+                lstpermisosactualizar.DataTextField = ds.Tables[0].Columns["Description"].ToString();
+                lstpermisosactualizar.DataValueField = ds.Tables[0].Columns["Id"].ToString();
+                lstpermisosactualizar.DataSource = ds.Tables[0];
+                lstpermisosactualizar.DataBind();
+                con.Close();
             }
         }
 
@@ -31,19 +49,12 @@ namespace TP_Integrador_Master
         {
             try
             {
+                ///////////////////////BUSCAR USUARIO
                 string usuario = txt_buscar_usuario.Text;
-                
-
-                if (String.IsNullOrEmpty(usuario))
-                {
-                    usuario = "";
-                }
-
-
                 string qry = "SELECT  U.UserName, P.Description " +
-                    "FROM  Users U LEFT JOIN UserPrivileges ON U.Id = UserPrivileges.UserID INNER JOIN " +
-                    "Privileges P ON UserPrivileges.PrivilegeID = P.Id " +
-                    "where U.UserName like '%" + usuario + "%' and " +
+                    "FROM  Users U LEFT JOIN UserPrivileges UP ON U.Id = UP.UserID LEFT JOIN " +
+                    "Privileges P ON UP.PrivilegeID = P.Id " +
+                    "where U.UserName = '" + usuario + "' and " +
                     "U.is_deleted=0;";
 
                 SqlCommand sqlCmd = new SqlCommand(qry, con);
@@ -51,38 +62,165 @@ namespace TP_Integrador_Master
                 SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlCmd);
                 DataTable dt = new DataTable();
                 dataAdapter.Fill(dt);
-                StringBuilder sb = new StringBuilder();
-                sb.Append("<table class=\"table  table-striped table-bordered text-center mt-4\">");
-                sb.Append("<thead >");
-                sb.Append("<tr class=\"table-dark\">");
-                sb.Append("<th scope=\"col\">Usuario</th>");
-                sb.Append("<th scope=\"col\">Permiso</th>");
-                sb.Append("</tr>");
-                sb.Append("</thead>");
-                sb.Append("<tbody>");
-
-                foreach (DataRow dr in dt.Rows)
+                if (dt.Rows.Count == 0)
                 {
-                    sb.Append("<tr>");
-                    foreach (DataColumn dc in dt.Columns)
-                    {
-                        sb.Append("<td>");
-                        sb.Append(dr[dc.ColumnName].ToString());
-                        sb.Append("</td>");
-                    }
-
-                    sb.Append("</tr>");
+                    text_usuario.Text = "";
+                    txt_permiso.Text = "";
                 }
-                sb.Append("</tbody>");
-                sb.Append("</table>");
+                else
+                {
+                    text_usuario.Text = dt.Rows[0][0].ToString();
+                    txt_permiso.Text = dt.Rows[0][1].ToString();
+                    label_error_edit.Text = "";
+                    label_error_borrar.Text = "";
+                }
                 con.Close();
-
-                tablaUsuarios.Controls.Add(new Label { Text = sb.ToString() });
             }
             catch (Exception ex)
             {
 
                 throw ex;
+            }
+        }
+
+        protected void crear_Click(object sender, EventArgs e)
+        {
+            ///////////////////////////Validación valores
+            bool error = false;
+            if (string.IsNullOrEmpty(txt_alta_usuario.Text))
+            {
+                label_error_nombre.Text = "Error! Nombre de usuario no valido";
+                error = true;
+            }
+            if (txt_alta_password.Text != txt_alta_password_confirm.Text || string.IsNullOrEmpty(txt_alta_password.Text))
+            {
+                label_error_pass.Text = "Error! Verifique que las contraseña sean correctas";
+                error = true;
+            }
+            if (!error)
+            {
+                try
+                {
+                    con.Open();
+                    ///////////////////////////Inserta Usuario
+                    string qry =
+                        "IF NOT EXISTS (SELECT 1 FROM USERS WHERE USERNAME='" + txt_alta_usuario.Text + "') " +
+                        "BEGIN " +
+                        "INSERT INTO USERS(Id, UserName, Salt, PasswordHash, is_deleted) " +
+                        "VALUES(NEWID(), '" + txt_alta_usuario.Text + "', '" + txt_alta_password.Text + "', " +
+                        "HASHBYTES('MD5', '" + txt_alta_password.Text + "'), 0); " +
+                        "END ";
+                    SqlCommand sqlCmd = new SqlCommand(qry, con);
+                    Int32 result = (Int32)sqlCmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        ///////////////////////////Inserta USUARIOPERMISO
+                        ///
+                        qry =
+                            "IF EXISTS(SELECT 1 FROM USERS WHERE USERNAME = '" + txt_alta_usuario.Text + "') " +
+                            "BEGIN " +
+                            "DECLARE @USUARIO UNIQUEIDENTIFIER = (SELECT ID FROM Users WHERE USERNAME = '" + txt_alta_usuario.Text + "'); " +
+                            "DECLARE @PERMISO UNIQUEIDENTIFIER = '" + lstpermisoscrear.SelectedValue.ToString() + "'; " +
+                            "INSERT INTO UserPrivileges(Id, UserID, PrivilegeID) " +
+                            "VALUES(NEWID(), @USUARIO, @PERMISO); " +
+                            "END";
+                        sqlCmd = new SqlCommand(qry, con);
+                        result = (Int32)sqlCmd.ExecuteNonQuery();
+
+                        label_error_nombre.Text = "";
+                        label_error_pass.Text = "";
+                        txt_alta_password.Text = "";
+                        txt_alta_password_confirm.Text = "";
+                        txt_alta_usuario.Text = "";
+                    }
+                    else
+                    {
+                        label_error_nombre.Text = "Usuario ya existe";
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+        }
+
+        protected void editar_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(text_usuario.Text))
+            {
+                label_error_edit.Text = "Seleccione un usuario";
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    ///////////////////////////Actualizar roles Usuario
+                    string qry =
+                        "DECLARE @USUARIO UNIQUEIDENTIFIER = (SELECT ID FROM Users WHERE USERNAME = '" + text_usuario.Text + "'); " +
+                        "DECLARE @PERMISO UNIQUEIDENTIFIER = '" + lstpermisosactualizar.SelectedValue.ToString() + "'; " +
+                        "update UserPrivileges " +
+                        "set PrivilegeID=@PERMISO " +
+                        "where UserID=@USUARIO;";
+                    SqlCommand sqlCmd = new SqlCommand(qry, con);
+                    Int32 result = (Int32)sqlCmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        label_error_edit.Text = "Usuario " + text_usuario.Text + " modificado";
+                        text_usuario.Text = "";
+                        txt_permiso.Text = "";
+                    }
+                    else
+                    {
+                        label_error_edit.Text = "Error al borrar";
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+        }
+        protected void borrar_Click(object sender, EventArgs e)
+        {
+            //////////////////////Valida Usuario Existe
+            if (string.IsNullOrEmpty(text_usuario.Text))
+            {
+                label_error_borrar.Text = "Seleccione un usuario";
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    ///////////////////////////Soft delete Usuario
+                    string qry =
+                        "update users " +
+                        "set is_deleted = 1 " +
+                        "where username = '" + text_usuario.Text + "';";
+                    SqlCommand sqlCmd = new SqlCommand(qry, con);
+                    Int32 result = (Int32)sqlCmd.ExecuteNonQuery();
+                    if (result == 1)
+                    {
+                        label_error_borrar.Text = "Usuario " + text_usuario.Text + " borrado";
+                        text_usuario.Text = "";
+                        txt_permiso.Text = "";
+                    }
+                    else
+                    {
+                        label_error_borrar.Text = "Error al borrar";
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
             }
         }
     }
